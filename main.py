1f2b08370aa90332156a14f154b12bf2e0370341
golony6449 <bb0162@live.co.kr>
2016-10-15 10:21:01 +0900

    Programming parser
    공지사항 파서 코딩중
    
    예정
    --존재하지 않는(미작성) 포스트 넘버 파악 후 처리

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-
+#-*- coding: utf-8 -*-
 
 import urllib
 from bs4 import BeautifulSoup
 
-url='http://cs.gnu.ac.kr/sub02/06.php?id=830&mode=read'
-html = urllib.urlopen(url)
-soup = BeautifulSoup(html,'html5lib',from_encoding='utf-8')
+def main():
 
-title_list=soup.find_all('td','tdleft')
+    for id in range(822,830):
 
-print
+        url='http://cs.gnu.ac.kr/sub02/06.php?id='+str(id)+'&mode=read'
+        html = urllib.urlopen(url)
+        soup = BeautifulSoup(html,'html5lib',from_encoding='utf-8')
+
+        title_list=soup.find_all('td','tdleft')
+        noti_title=parse_title(title_list)
+
+        print id,'번째 공지사항: ',noti_title
+
+def parse_title(target):
+    source=str(target[0]).decode('utf=8')
+    ouput=''
+    start=0
+    end=0
+
+    for pointer in range(len(source)-1):
+        if source[pointer] == '>':
+            start = pointer+1
+        try:
+            if source[pointer] == '<' and pointer > start:
+                end = pointer
+        except:
+            pass
+    return source[start:end]
+
+
+if __name__=='__main__':
+    main()
