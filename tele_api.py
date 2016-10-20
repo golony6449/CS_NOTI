9bf9c995f4986849804cc76d7b7d2e14932da200
golony6449 <bb0162@live.co.kr>
2016-10-20 19:21:06 +0900

    Bug fix
    BUG FIX
    -- Now, it can send FULL URL
    
    ADD
    -- Send notification text with URL

--- tele_api.py
+++ tele_api.py
@@ -1,4 +1,5 @@
 import urllib2
+import requests
 
 url='https://api.telegram.org/bot'
 api_key='257446028:AAGgXliWqKyOXoGQ8oFm6rmyiB4YVdZ-ovA'
@@ -10,13 +11,16 @@ class Telegram():
         pass
 
     def send(self,text=''):
-        req = urllib2.Request(url + api_key + '/sendMessage')
-        req.add_data(ch_id+'&text='+text.encode('utf-8'))
-        socket=urllib2.urlopen(req)
+        #req = urllib2.Request(url + api_key + '/sendMessage')
+        #req.add_data(ch_id+'&text='+text.encode('utf-8'))
+        #socket=urllib2.urlopen(req)
+        data={'chat_id':'@CS_NOTI','text':text}
+        socket=requests.post(url=url+api_key+'/sendMessage',data=data)
 
     def notification(self,text='',path=''):
-        self.send(text)
-        self.send(path)
+        #self.send(text)
+        #self.send(path)
+        self.send(text+'\n'+path)
 
     def test(self):
         req=urllib2.Request(url+api_key+'/sendMessage')
