0a581d05f2aba00ecb0f7d687d1ba5eb4e059908
park seong heum <bb0162@live.co.kr>
2019-12-04 22:27:44 +0900

    카테고리 별 푸시 메시지 전송 기능 추가

--- module/firebase.py
+++ module/firebase.py
@@ -5,8 +5,8 @@ from firebase_admin import credentials, messaging
 from firebase_admin import firestore
 import os
 
-# cred = credentials.Certificate(os.environ['GOOGLE_SERVICE_ACCOUNT'])
-cred = credentials.Certificate('c:/gnu-noti-firebase-adminsdk.json')
+cred = credentials.Certificate(os.environ['GOOGLE_SERVICE_ACCOUNT'])
+
 try:
     firebase_admin.initialize_app(cred)
 except ValueError:
@@ -15,6 +15,14 @@ db = firestore.client()
 
 
 def register_new_noti(category, title, url):
+    ch = None
+    if category == '기관공지':
+        ch = 'agency'
+    elif category == 'HOT NEWS':
+        ch = 'gnu'
+    else:
+        raise Exception("Wrong Category Parameter")
+
     try:
         _ = os.environ['DEBUG']
         id_docu = db.collection('environ').document('dev_id')
@@ -25,6 +33,7 @@ def register_new_noti(category, title, url):
             'id': id + 1
         })
     except KeyError:
+        # 혼합 리스트
         id_docu = db.collection('environ').document('mix_id')
         id = id_docu.get().to_dict()['id']
 
@@ -40,16 +49,41 @@ def register_new_noti(category, title, url):
         'url': url
     })
 
+    # 채널에 따른 추가
+    id_target_ch_docu = db.collection('environ').document(ch)
+    target_ch_id = id_target_ch_docu.get().to_dict()['id']
 
-def send_notification(title, body, topic=''):
-    if topic == '':
-        try:
-            _ = os.environ['DEBUG']
-            topic = 'dev'
-        except KeyError:
-            topic = 'mix'
+    new_target_ch_docu = db.collection(ch).document(str(target_ch_id))
+    id_target_ch_docu.update({
+        'id': target_ch_id + 1
+    })
+
+    new_target_ch_docu.set({
+        'id': target_ch_id,
+        'category': category,
+        'title': title,
+        'url': url
+    })
+
+
+def send_notification(title, body):
+    topics = []
+
+    try:
+        _ = os.environ['DEBUG']
+        topics.append('dev')
+    except KeyError:
+        topics.append('mix')
+
+        if title == 'HOT NEWS':
+            topics.append('gnu')
+
+        if title == '기관공지':
+            topics.append('agency')
 
     noti = messaging.Notification(title='GNU-NOTI ({})'.format(title), body=body)
-    message = messaging.Message(notification=noti, topic=topic)
-    response = messaging.send(message)
-    print("Send Notification Successfully")
+
+    for t in topics:
+        message = messaging.Message(notification=noti, topic=topics)
+        response = messaging.send(message)
+        print("{}: Send Notification Successfully".format(t))
